f9e7f8cf0f0456016c32568a3a6a18a8ae63d2af
Song Fu <sofu@Songs-MacBook-Pro.local>
2017-06-23 18:27:14 +0800

    add some files for test

---
+++ index.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+    function getRandomNumber() {
+
+        var random = Math.floor(Math.random() * 100);
+        if (random >= 100)
+            throw new Error("Error");
+        return random;
+    }
+    exports.getRandomNumber = getRandomNumber;
+  }());
